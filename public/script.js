f21b4cbd9466fe5027b3d79841c82010e89b010a
rnfyatendra-lab <rnf.yatendra@gmail.com>
2026-01-21 07:21:16 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,13 +1,14 @@
 const sendBtn = document.getElementById('sendBtn');
+const counter = document.getElementById('counter');
 
 sendBtn.onclick = async () => {
   sendBtn.disabled = true;
   sendBtn.innerText = "Sending…";
 
   const res = await fetch('/send', {
-    method:'POST',
-    headers:{'Content-Type':'application/json'},
-    body:JSON.stringify({
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
       senderName: senderName.value,
       email: email.value,
       password: password.value,
@@ -18,12 +19,17 @@ sendBtn.onclick = async () => {
   });
 
   const data = await res.json();
-  alert(data.message);
+  alert(data.message || "Done");
+
+  if (data.used !== undefined) {
+    counter.innerText = `Used ${data.used} / ${data.limit}`;
+  }
 
   sendBtn.disabled = false;
   sendBtn.innerText = "Send";
 };
 
-function logout(){
-  fetch('/logout',{method:'POST'}).then(()=>location.href='/');
+function logout() {
+  fetch('/logout', { method: 'POST' })
+    .then(() => location.href = '/');
 }
