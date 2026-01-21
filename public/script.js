1301fd46770bbfc550b0d69f43f596e895aef6c3
rnfyatendra-lab <rnf.yatendra@gmail.com>
2026-01-21 09:02:00 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,24 +4,21 @@ sendBtn.onclick = async () => {
   sendBtn.disabled = true;
   sendBtn.innerText = "Sending…";
 
-  try {
-    const res = await fetch('/send', {
-      method:'POST',
-      headers:{'Content-Type':'application/json'},
-      body:JSON.stringify({
-        senderName: senderName.value,
-        email: email.value,
-        password: password.value,
-        subject: subject.value,
-        message: message.value,
-        recipients: recipients.value
-      })
-    });
-    const data = await res.json();
-    alert(data.message);
-  } catch {
-    alert("Network error");
-  }
+  const res = await fetch('/send', {
+    method:'POST',
+    headers:{'Content-Type':'application/json'},
+    body:JSON.stringify({
+      senderName: senderName.value,
+      email: email.value,
+      password: password.value,
+      subject: subject.value,
+      message: message.value,
+      recipients: recipients.value
+    })
+  });
+
+  const data = await res.json();
+  alert(data.message);
 
   sendBtn.disabled = false;
   sendBtn.innerText = "Send";
